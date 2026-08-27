4b0dde5df5c5f003502d4a85702cf48a3884c649
alaka <alakazoidiac@gmail.com>
2026-08-28 08:43:12 +1200

    configure vimtex options

--- nvim/after/ftplugin/tex.lua
+++ nvim/after/ftplugin/tex.lua
@@ -1,4 +1,12 @@
 local map = vim.keymap.set
+local opt = vim.opt_local
+
+opt.wrap = true
+opt.linebreak = true
+opt.breakindent = true
+opt.spell = true
+-- opt.conceallevel = 2
+-- opt.concealcursor = "nc"
 
 map("n", "<leader>b", "<cmd>VimtexCompile<cr>", {
     buffer = true,
