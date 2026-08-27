e0986c75cd95f4b8a12041d15c9bbc118678ebd1
alaka <alakazoidiac@gmail.com>
2026-08-28 06:02:56 +1200

    add vimtex comments

--- nvim/lua/plugins/latex.lua
+++ nvim/lua/plugins/latex.lua
@@ -3,11 +3,15 @@ return {
         "lervag/vimtex",
         lazy = false,
         init = function()
+            -- Use the general viewer backend to avoid an extra cmd window on Windows
             vim.g.vimtex_view_method = "general"
             vim.g.vimtex_view_general_viewer = 'start ""'
+
+            -- Launch Sioyek with forward and inverse SyncTeX support
             vim.g.vimtex_view_general_options =
                 [[sioyek --inverse-search "nvim --headless -c \"VimtexInverseSearch %2 '%1'\"" --forward-search-file @tex --forward-search-line @line @pdf]]
 
+            -- Use pdfLaTeX by default for fast builds and reliable SyncTeX
             vim.g.vimtex_compiler_latexmk_engines = {
                 _ = "-pdf",
             }
