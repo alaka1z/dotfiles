45d131857a5b49dec8bdd4ce8912b3775d91f97a
alaka <alakazoidiac@gmail.com>
2026-08-28 05:57:53 +1200

    add latex workflow

--- nvim/lua/plugins/latex.lua
+++ nvim/lua/plugins/latex.lua
@@ -3,10 +3,13 @@ return {
         "lervag/vimtex",
         lazy = false,
         init = function()
-            vim.g.vimtex_callback_progpath = "nvim"
-            vim.g.vimtex_view_method = "sioyek"
+            vim.g.vimtex_view_method = "general"
+            vim.g.vimtex_view_general_viewer = 'start ""'
+            vim.g.vimtex_view_general_options =
+                [[sioyek --inverse-search "nvim --headless -c \"VimtexInverseSearch %2 '%1'\"" --forward-search-file @tex --forward-search-line @line @pdf]]
+
             vim.g.vimtex_compiler_latexmk_engines = {
-                _ = "-lualatex",
+                _ = "-pdf",
             }
         end,
     },
