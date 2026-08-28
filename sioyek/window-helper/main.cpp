75a9cd5a67fe4ca755a50c903f83e21369495edf
alaka <alakazoidiac@gmail.com>
2026-08-29 07:18:36 +1200

    add sioyek window helper

---
+++ sioyek/window-helper/main.cpp
@@ -0,0 +1,100 @@
+#include <windows.h>
+#include <dwmapi.h>
+#include <string>
+
+#pragma comment(lib, "dwmapi.lib")
+
+static HWND sioyek_window = nullptr;
+
+BOOL CALLBACK find_sioyek_window(HWND hwnd, LPARAM)
+{
+    if (!IsWindowVisible(hwnd)) {
+        return TRUE;
+    }
+
+    DWORD process_id = 0;
+    GetWindowThreadProcessId(hwnd, &process_id);
+
+    HANDLE process = OpenProcess(
+        PROCESS_QUERY_LIMITED_INFORMATION,
+        FALSE,
+        process_id
+    );
+
+    if (!process) {
+        return TRUE;
+    }
+
+    wchar_t path[MAX_PATH];
+    DWORD size = MAX_PATH;
+
+    if (QueryFullProcessImageNameW(process, 0, path, &size)) {
+        std::wstring executable(path);
+
+        const auto separator = executable.find_last_of(L"\\/");
+        if (separator != std::wstring::npos) {
+            executable = executable.substr(separator + 1);
+        }
+
+        if (_wcsicmp(executable.c_str(), L"sioyek.exe") == 0) {
+            sioyek_window = hwnd;
+            CloseHandle(process);
+            return FALSE;
+        }
+    }
+
+    CloseHandle(process);
+    return TRUE;
+}
+
+int WINAPI wWinMain(
+    HINSTANCE,
+    HINSTANCE,
+    PWSTR,
+    int
+)
+{
+    EnumWindows(find_sioyek_window, 0);
+
+    if (!sioyek_window) {
+        return 0;
+    }
+
+    LONG_PTR style = GetWindowLongPtrW(
+        sioyek_window,
+        GWL_STYLE
+    );
+
+    style |= WS_MAXIMIZEBOX;
+
+    SetWindowLongPtrW(
+        sioyek_window,
+        GWL_STYLE,
+        style
+    );
+
+    SetWindowPos(
+        sioyek_window,
+        nullptr,
+        0,
+        0,
+        0,
+        0,
+        SWP_NOMOVE |
+        SWP_NOSIZE |
+        SWP_NOZORDER |
+        SWP_NOACTIVATE |
+        SWP_FRAMECHANGED
+    );
+
+    constexpr DWORD caption_color = RGB(255, 255, 255);
+
+    DwmSetWindowAttribute(
+        sioyek_window,
+        35,
+        &caption_color,
+        sizeof(caption_color)
+    );
+
+    return 0;
+}
