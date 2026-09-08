11b62cc7cec8a82538c113a0f636acaa3efa7e9e
alaka <alakazoidiac@gmail.com>
2026-09-08 18:40:52 +1200

    Clean up WezTerm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -70,27 +70,6 @@ config.keys = {
             window:maximize()
         end),
     },
-
-    -- Work around a Windows title-bar flash when returning to fullscreen
-    --
-    -- Enter fullscreen from a normally decorated window, then restore the
-    -- titleless RESIZE configuration while leaving fullscreen
-    -- {
-    --     key = "Enter",
-    --     mods = "ALT",
-    --     action = wezterm.action_callback(function(window, pane)
-    --         local overrides = window:get_config_overrides() or {}
-    --
-    --         if window:get_dimensions().is_full_screen then
-    --             overrides.window_decorations = "RESIZE"
-    --         else
-    --             overrides.window_decorations = "TITLE|RESIZE"
-    --         end
-    --
-    --         window:set_config_overrides(overrides)
-    --         window:perform_action(wezterm.action.ToggleFullScreen, pane)
-    --     end),
-    -- },
 }
 
 -- Appearance
